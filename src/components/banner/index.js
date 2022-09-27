15213e1468f22d9344b952277e6d6443e4982c58
thuong1804 <{thuongtvt30@gmail.com}>
2022-09-27 22:30:25 +0700

    continue content

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -20,7 +20,7 @@ function Banner()
                 <Col className='text-col'>
                 <h4>Web Designer</h4>
                 <h1>Alex Smith</h1>
-                <p> Fusce tempor magna mi, non egestas velit ultricies nec. 
+                <p id='text-p-banner'> Fusce tempor magna mi, non egestas velit ultricies nec. 
                     Aenean convallis, risus non condimentum gravida, odio mauris 
                     ullamcorper felis, ut venenatis purus ex eu mi. Quisque imperdiet
                      lacinia urna, a placerat sapien pretium eu.</p>
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Col, Row } from "reactstrap";
 import "./content.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faStore } from "@fortawesome/free-solid-svg-icons";
+import { faMobileScreen } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+import testimonial_1 from "../../assets/images/testimonial-1.jpg";
+import testimonial_2 from "../../assets/images/testimonial-2.jpg";
 
 function Content() {
   return (
@@ -10,25 +16,80 @@ function Content() {
         <h2>What I Do </h2>
       </div>
       <div className="row-content">
-      <Row>
-        <Col className="Copywrite">
-         
-          <h4>Copywrite</h4>
-          <p>Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. 
-            Mauris convallis dictum elit id volutpat. 
-            Vivamus blandit, dolor vitae lacin
-            ia maximus, risus velit vehicula odio, 
-            a tincidunt turpis turpis tempus ex.</p>
-          
-        </Col>
-        <Col className="Ecommerce">
-          <h4>Ecommerce</h4>
-          <p>Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. 
-            Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacin
-            ia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.</p>
-        </Col>
-      </Row>
+        <Row>
+          <Col className="Copywrite">
+            <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+            <h4>Copywrite</h4>
+            <p id="text-p-Copywrite">
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+              vitae lacin ia maximus, risus velit vehicula odio, a tincidunt
+              turpis turpis tempus ex.
+            </p>
+          </Col>
+          <Col className="Ecommerce">
+            <FontAwesomeIcon icon={faStore}></FontAwesomeIcon>
+            <h4>Ecommerce</h4>
+            <p id="text-p-Ecommerce">
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+              vitae lacin ia maximus, risus velit vehicula odio, a tincidunt
+              turpis turpis tempus ex.
+            </p>
+          </Col>
+        </Row>
       </div>
+      <div className="row-content-down">
+        <Row>
+          <Col className="Copywrite">
+            <FontAwesomeIcon icon={faMobileScreen}></FontAwesomeIcon>
+            <h4>Copywrite</h4>
+            <p id="text-p-Copywrite">
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+              vitae lacin ia maximus, risus velit vehicula odio, a tincidunt
+              turpis turpis tempus ex.
+            </p>
+          </Col>
+          <Col className="Ecommerce">
+            <FontAwesomeIcon icon={faVolumeHigh}></FontAwesomeIcon>
+            <h4>Ecommerce</h4>
+            <p id="text-p-Ecommerce">
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+              vitae lacin ia maximus, risus velit vehicula odio, a tincidunt
+              turpis turpis tempus ex.
+            </p>
+          </Col>
+        </Row>
+      </div>
+      <div className="Testimonials">
+        <h2 className="testimonials-h2">Testimonials</h2>
+      </div>
+      <div className="row_banner_img">
+      <Row>
+          <Col className="Copywrite">
+            <img src={testimonial_1}></img>
+            <h4>Copywrite</h4>
+            <p id="text-p-Copywrite">
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+              vitae lacin ia maximus, risus velit vehicula odio, a tincidunt
+              turpis turpis tempus ex.
+            </p>
+          </Col>
+          <Col className="Ecommerce">
+            <img src={testimonial_2}></img>
+            <h4>Ecommerce</h4>
+            <p id="text-p-Ecommerce">
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+              vitae lacin ia maximus, risus velit vehicula odio, a tincidunt
+              turpis turpis tempus ex.
+            </p>
+          </Col>
+        </Row>
+        </div>
     </div>
   );
 }
